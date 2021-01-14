ffc1290b28f6799dce266c5627399083a53551f3
Diana Solano <dsolano@kubo.co>
2021-01-13 21:03:33 -0500

    [Fix] Country and version file names

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { SystemCity } from './system-city.model';
-import { SystemCountry } from './system-country';
+import { SystemCountry } from './system-country.model';
 import { UserSettingNotification } from './user-setting-notification.model';
 import { UserToken } from './user-token.model';
 import { User } from './user.model';
-import { SystemVersion } from './system-version';
+import { SystemVersion } from './system-version.model';
 
 export default [
   User,
--- system-city.model.ts
+++ system-city.model.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { SystemCountry } from './system-country';
+import { SystemCountry } from './system-country.model';
 import { Options } from './options.model';
 
 @Entity()
